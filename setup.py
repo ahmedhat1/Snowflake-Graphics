7498f1075173725128758ded112591b3099b303f
ahmedhat1 <ahmed.sadekibrahim@gmail.com>
2022-12-04 21:56:49 +0100

    fixed bug (misspelled find_packages)

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
-from setuptools import find_pacakges
+from setuptools import find_packages
 
 setup(
       name='snowflake',
       version='0.1',
       author='Ahmed Ibrahim',
       author_email='ahmed.medhat.ibrahim@fau.de',
-      packages=find_pacakges(),
-      install_requires=["numpy","turtles"]
+      packages=find_packages(),
+      install_requires=["numpy","turtles "]
      )
